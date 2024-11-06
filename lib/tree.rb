a07dcf80130a189a955e7b41252a337c53c4c41e
Lincoln Digby <lincolndigby0@gmail.com>
2024-11-05 20:57:35 -0600

    Add level_order, preorder, inorder, and postorder

--- lib/tree.rb
+++ lib/tree.rb
@@ -2,12 +2,13 @@ require_relative "node"
 
 class Tree
   def initialize(array)
-    array.uniq!.sort!
+    array.uniq! unless array.length == 1
+    array.sort!
     @root = build_tree(array)
   end
 
   def build_tree(array, first = 0, last = array.length - 1)
-    return if first > last
+    return if first > last || array.nil?
 
     mid = (first + last) / 2
     Node.new(array[mid], build_tree(array, first, mid - 1), build_tree(array, mid + 1, last))
@@ -17,5 +18,117 @@ class Tree
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  def insert(value, current_node = @root)
+    return Node.new(value) if current_node.nil?
+
+    return Node.new(value) if current_node.data == value
+
+    if value > current_node.data
+      current_node.right = insert(value, current_node.right)
+    else
+      current_node.left = insert(value, current_node.left)
+    end
+
+    current_node
+  end
+
+  def get_successor(curr)
+    curr = curr.right
+    curr = curr.left while !curr.nil? && !curr.left.nil?
+    curr
+  end
+
+  def delete(value, curr = @root)
+    return curr if curr.nil?
+
+    if curr.data > value
+      curr.left = delete(value, curr.left)
+    elsif curr.data < value
+      curr.right = delete(value, curr.right)
+    else
+      if curr.left.nil?
+        return curr.right
+      elsif curr.right.nil?
+        return curr.left
+      end
+
+      succ = get_successor(curr)
+      curr.data = succ.data
+      curr.right = delete(succ.data, curr.right)
+    end
+
+    curr
+  end
+
+  def find(value, curr = @root)
+    return nil if curr.nil?
+
+    if value < curr.data
+      find(value, curr.left)
+    elsif value > curr.data
+      find(value, curr.right)
+    else
+      curr
+    end
+  end
+
+  def level_order_iterative
+    return if @root.nil?
+
+    arr = []
+
+    queue = []
+
+    queue.push(@root)
+
+    until queue.empty?
+      curr = queue[0]
+      block_given? ? (yield curr.data) : arr << curr.data
+      queue.push(curr.left) unless curr.left.nil?
+      queue.push(curr.right) unless curr.right.nil?
+      queue.shift
+    end
+    block_given? ? nil : arr
+  end
+
+  def preorder(curr = @root, &block)
+    return if curr.nil?
+
+    yield curr.data
+    preorder(curr.left, &block)
+    preorder(curr.right, &block)
+    curr
+  end
+
+  def inorder(curr = @root, &block)
+    return if curr.nil?
+
+    inorder(curr.left, &block)
+    yield curr.data
+    inorder(curr.right, &block)
+    curr
+  end
+
+  def postorder(curr = @root, &block)
+    return if curr.nil?
+
+    postorder(curr.left, &block)
+    postorder(curr.right, &block)
+    yield curr.data
+    curr
+  end
+
+  def height(node)
+  end
+
+  def depth(node)
+  end
+
+  def balanced?
+  end
+
+  def rebalance
   end
 end
--- main.rb
+++ main.rb
@@ -3,3 +3,21 @@ require_relative "lib/tree"
 tree = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 
 p tree.pretty_print
+
+tree.insert(345)
+
+p tree.pretty_print
+
+tree.delete(8)
+
+p tree.pretty_print
+
+p tree.find(10)
+
+# tree.level_order_iterative { |v| p v }
+puts "Preorder: \n"
+tree.preorder { |v| p v }
+puts "Inorder: \n"
+tree.inorder { |v| p v }
+puts "Postorder: \n"
+tree.postorder { |v| p v }
